accbfbf9e2256fb1f14eea87479ad6b2e1f41c31
Aarav Dhawan <aaravdhawan25@gmail.com>
2026-02-02 20:48:50 -0500

    paths change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auto/New/RedFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/auto/New/RedFar.java
@@ -116,7 +116,7 @@ public class RedFar extends LinearOpMode {
                 .splineToLinearHeading(new Pose2d(-23.3,19,Math.toRadians(360-250)), Math.toRadians(360-15),
                         new TranslationalVelConstraint(40))
 
-                .splineToSplineHeading(new Pose2d(-12,52,Math.toRadians(360-270)),Math.toRadians(360-270),
+                .splineToSplineHeading(new Pose2d(-12,48,Math.toRadians(360-270)),Math.toRadians(360-270),
                         new TranslationalVelConstraint(40))
                 //.splineToLinearHeading(new Pose2d(-14.5,-49.3,Math.toRadians(270)), Math.toRadians(200))
                 .waitSeconds(0.1);
